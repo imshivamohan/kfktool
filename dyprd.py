896417526eb78c4e512a39bfa5265f50c31b920a
imshivamohan <87896804+imshivamohan@users.noreply.github.com>
2023-06-05 11:13:36 +0530

    Update dyprd.py

--- dyprd.py
+++ dyprd.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from confluent_kafka import Producer
 from confluent_kafka.serialization import SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -19,78 +18,69 @@ def create_serializer_function(class_name):
 
 def delivery_report(err, event):
     if err is not None:
-        st.error('Delivery failed on reading for {}: {}'.format(event.key().decode("utf8"), err))
+        print('Delivery failed on reading for {}: {}'.format(event.key().decode("utf8"), err))
     else:
-        st.success('Temp reading for {} produced to {}'.format(event.key().decode("utf8"), event.topic()))
-
-# Streamlit UI
-st.title('Kafka Producer with Dynamic Schema')
+        print('Temp reading for {} produced to {}'.format(event.key().decode("utf8"), event.topic()))
 
 # Input JSON schema
-st.header('JSON Schema')
-schema_text = st.text_area('Enter JSON Schema')
+schema = {
+    "title": "Employee",
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "age": {"type": "integer"}
+    },
+    "required": ["name", "age"]
+}
 
 # Dynamic Dictionary Data
-st.header('Dynamic Dictionary Data')
-data_text = st.text_area('Enter Dynamic Dictionary Data (JSON format)')
+dynamic_dictionary_data = [
+    {"name": "John", "age": 30},
+    {"name": "Jane", "age": 25}
+]
 
-# Submit Button
-submit_button = st.button('Produce Messages')
+# Generate dynamic class, JSON schema, and serializer function
+DynamicClass = create_dynamic_class(schema)
+json_schema = create_json_schema_class(schema)
+serializer_function = create_serializer_function(DynamicClass.__name__)
 
-if submit_button:
-    try:
-        # Parse JSON schema
-        schema = json.loads(schema_text)
+# Kafka producer configuration
+config = {
+    'bootstrap.servers': '<kafka-bootstrap-servers>',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': '<username>',
+    'sasl.password': '<password>',
+    'ssl.ca.location': '/path/to/ca.crt',
+    'client.id': 'python-producer'
+}
 
-        # Generate dynamic class, JSON schema, and serializer function
-        DynamicClass = create_dynamic_class(schema)
-        json_schema = create_json_schema_class(schema)
-        serializer_function = create_serializer_function(DynamicClass.__name__)
+# Schema Registry configuration
+sr_config = {
+    'url': '<schema-registry-url>',
+    'basic.auth.user.info': '<username>:<password>'
+}
 
-        # Parse dynamic dictionary data
-        dynamic_dictionary_data = json.loads(data_text)
+# Create Schema Registry client and JSON serializer
+schema_registry_client = SchemaRegistryClient(sr_config)
+json_serializer = JSONSerializer(json_schema, schema_registry_client, eval(serializer_function))
 
-        # Kafka producer configuration
-        config = {
-            'bootstrap.servers': '<kafka-bootstrap-servers>',
-            'security.protocol': 'SASL_SSL',
-            'sasl.mechanism': 'PLAIN',
-            'sasl.username': '<username>',
-            'sasl.password': '<password>',
-            'ssl.ca.location': '/path/to/ca.crt',
-            'client.id': 'python-producer'
-        }
+# Kafka producer setup
+producer = Producer(config)
+topic = 'temp_readings'
 
-        # Schema Registry configuration
-        sr_config = {
-            'url': '<schema-registry-url>',
-            'basic.auth.user.info': '<username>:<password>'
-        }
+# Produce messages
+for dynamic_data in dynamic_dictionary_data:
+    dynamic_object = DynamicClass(**dynamic_data)
+    producer.produce(
+        topic=topic,
+        key=str(dynamic_object.name),
+        value=json_serializer(
+            dynamic_object,
+            SerializationContext(topic, MessageField.VALUE)
+        ),
+        on_delivery=delivery_report
+    )
 
-        # Create Schema Registry client and JSON serializer
-        schema_registry_client = SchemaRegistryClient(sr_config)
-        json_serializer = JSONSerializer(json_schema, schema_registry_client, eval(serializer_function))
-
-        # Kafka producer setup
-        producer = Producer(config)
-        topic = 'temp_readings'
-
-        # Produce messages
-        for dynamic_data in dynamic_dictionary_data:
-            dynamic_object = DynamicClass(**dynamic_data)
-            producer.produce(
-                topic=topic,
-                key=str(dynamic_object.name),
-                value=json_serializer(
-                    dynamic_object,
-                    SerializationContext(topic, MessageField.VALUE)
-                ),
-                on_delivery=delivery_report
-            )
-
-        # Flush producer
-        producer.flush()
-
-        st.success('Messages produced successfully!')
-    except Exception as e:
-        st.error('Error: {}'.format(e))
+# Flush producer
+producer.flush()
