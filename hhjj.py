20ce323b28d6164ce2542fa927c12c566b07e011
imshivamohan <87896804+imshivamohan@users.noreply.github.com>
2023-06-23 18:20:33 +0530

    Update hhjj.py

--- hhjj.py
+++ hhjj.py
@@ -1,3 +1,19 @@
+from jinja2 import Environment
+import json
+import requests
+
+
+def fetch_json_schema(url):
+    """
+    Fetches the JSON schema from the provided URL.
+    Returns the JSON schema as a dictionary.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    json_schema = response.json()
+    return json_schema
+
+
 def generate_java_pojos(json_schema_url):
     """
     Generates Java POJOs based on a JSON schema fetched from the provided URL.
@@ -6,6 +22,23 @@ def generate_java_pojos(json_schema_url):
     json_schema = fetch_json_schema(json_schema_url)
 
     pojo_code_snippets = {}
+
+    def generate_java_type(schema):
+        # Add your custom type mappings here
+        type_mappings = {
+            "string": "String",
+            "integer": "int",
+            "number": "double",
+            "boolean": "boolean",
+            "array": "List",
+        }
+        java_type = type_mappings.get(schema.get("type", "Object"), "Object")
+        if java_type == "List":
+            items = schema.get("items", {})
+            if "$ref" in items:
+                ref_class_name = items["$ref"].split("/")[-1]
+                java_type += f"<{ref_class_name}>"
+        return java_type
 
     def generate_class_code(class_name, properties):
         template_str = """
@@ -37,119 +70,8 @@ def generate_java_pojos(json_schema_url):
         return rendered_code
 
     def traverse_schema(schema, class_name):
-        if schema.get("type") == "object":
-            properties = schema.get("properties")
-            if properties:
-                pojo_code_snippets[class_name] = generate_class_code(class_name, properties)
-
-                for prop, prop_schema in properties.items():
-                    if "$ref" in prop_schema:
-                        ref_class_name = prop_schema["$ref"].split("/")[-1]
-                        traverse_schema(prop_schema, ref_class_name)
-                    elif "type" in prop_schema and prop_schema["type"] == "array":
-                        items_schema = prop_schema.get("items", {})
-                        if "$ref" in items_schema:
-                            ref_class_name = items_schema["$ref"].split("/")[-1]
-                            traverse_schema(items_schema, ref_class_name)
-
-    traverse_schema(json_schema, "NewGenPOJOClass")
-
-    return pojo_code_snippets
-
-
-
-#################
-
-import json
-from jinja2 import Environment, Template
-import requests
-
-def fetch_json_schema(url):
-    """
-    Fetches the JSON schema from the provided URL.
-    Returns the JSON schema as a dictionary.
-    """
-    response = requests.get(url)
-    response.raise_for_status()
-    json_schema = response.json()
-    return json_schema
-
-def generate_java_type(property_schema):
-    """
-    Generates the Java type for a given property schema,
-    considering custom type mappings and handling arrays.
-    """
-    # Custom type mappings (JSON type to Java type)
-    custom_type_mappings = {
-        "integer": "int",
-        "number": "double",
-        "string": "String",
-        "boolean": "boolean",
-        # Add more custom type mappings as needed
-    }
-
-    if "type" in property_schema:
-        prop_type = property_schema["type"]
-        if prop_type == "array":
-            items_schema = property_schema.get("items", {})
-            if "$ref" in items_schema:
-                ref_type = items_schema["$ref"].split("/")[-1]
-                return f"List<{ref_type}>"
-            elif "type" in items_schema:
-                item_type = generate_java_type(items_schema)
-                return f"List<{item_type}>"
-        elif prop_type == "object":
-            if "$ref" in property_schema:
-                ref_type = property_schema["$ref"].split("/")[-1]
-                return ref_type
-            else:
-                return "Object"
-        elif prop_type in custom_type_mappings:
-            return custom_type_mappings[prop_type]
-
-    # Default to Object type if no specific mapping found
-    return "Object"
-
-def generate_java_pojos(json_schema_url):
-    """
-    Generates Java POJOs based on a JSON schema fetched from the provided URL.
-    Returns a dictionary of generated Java code snippets where the key is the class name.
-    """
-    json_schema = fetch_json_schema(json_schema_url)
-
-    pojo_code_snippets = {}
-
-    def generate_class_code(class_name, properties):
-        template_str = """
-        public class {{ class_name }} {
-            {% for prop, prop_schema in properties.items() %}
-            private {{ generate_java_type(prop_schema) }} {{ prop }};
-            {% endfor %}
-
-            // Getters and Setters
-            {% for prop, prop_schema in properties.items() %}
-            public {{ generate_java_type(prop_schema) }} get{{ prop|capitalize }}() {
-                return {{ prop }};
-            }
-
-            public void set{{ prop|capitalize }}({{ generate_java_type(prop_schema) }} {{ prop }}) {
-                this.{{ prop }} = {{ prop }};
-            }
-            {% endfor %}
-        }
-        """
-
-        env = Environment()
-        env.filters["generate_java_type"] = generate_java_type
-        template = env.from_string(template_str)
-        rendered_code = template.render(class_name=class_name, properties=properties)
-
-        return rendered_code
-
-    def traverse_schema(schema, class_name):
-        if "type" in schema and schema["type"] == "object":
-            properties = schema.get("properties", {})
-            required_fields = schema.get("required", [])
+        properties = schema.get("properties")
+        if properties:
             pojo_code_snippets[class_name] = generate_class_code(class_name, properties)
 
             for prop, prop_schema in properties.items():
@@ -162,11 +84,10 @@ def generate_java_pojos(json_schema_url):
                         ref_class_name = items_schema["$ref"].split("/")[-1]
                         traverse_schema(items_schema, ref_class_name)
 
-    # Traverse the JSON schema and generate Java POJOs
-    class_name = json_schema.get("title", "NewGenPOJOClass")
-    traverse_schema(json_schema, class_name)
+    traverse_schema(json_schema, "NewGenPOJOClass")
 
     return pojo_code_snippets
+
 
 # URL for fetching the JSON schema
 json_schema_url = "https://example.com/json-schema"
